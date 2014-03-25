6220ff0f62d25fe9b1868b8c767cbb07636e0dc1
Richard Spindler <richard@lateralblast.com.au>
2014-03-25 14:43:26 +1100

    Added inet/init code and CIS reference for FreeBSD

--- lunar.sh
+++ lunar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Name:         lunar (Lockdown UNIX Analyse Report)
-# Version:      3.5.4
+# Version:      3.5.5
 # Release:      1
 # License:      Open Source
 # Group:        System
