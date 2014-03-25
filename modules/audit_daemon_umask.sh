8483bad60d8ed407accf73b92f3e8bfe396aea72
Richard Spindler <richard@lateralblast.com.au>
2014-03-25 15:34:38 +1100

    Added daemon umask and CIS reference for FreeBSD

--- modules/audit_daemon_umask.sh
+++ modules/audit_daemon_umask.sh
@@ -27,7 +27,7 @@
 # applications- consult vendor documentation for further information.
 # The default setting for Solaris is 022.
 #
-# Linux
+# Linux and FreeBSD
 #
 # Set the default umask for all processes started at boot time.
 # The settings in umask selectively turn off default permission when a file is
@@ -38,79 +38,93 @@
 # daemon process. The daemon process can manually override these settings if
 # these files need additional permission.
 #
-# Refer to Section 3.1 Page(s) 58-59 CIS CentOS Linux 6 Benchmark v1.0.0
+# Refer to Section 3.1 Page(s) 58-9 CIS CentOS Linux 6 Benchmark v1.0.0
+# Refer to Section 3.3 Page(s) 9-10 CIS FreeBSD Benchmark v1.0.5
 #.
 
 audit_daemon_umask () {
-  if [ "$os_name" = "SunOS" ]; then
-    if [ "$os_version" = "11" ]; then
-      funct_verbose_message "Daemon Umask"
-      umask_check=`svcprop -p umask/umask svc:/system/environment:init`
-      umask_value="022"
-      log_file="umask.log"
-      total=`expr $total + 1`
-      if [ "$umask_check" != "$umask_value" ]; then
-        log_file="$work_dir/$log_file"
-        if [ "$audit_mode" = 1 ]; then
-          score=`expr $score - 1`
-          echo "Warning:   Default service file creation mask not set to $umask_value [$score]"
-          funct_verbose_message "" fix
-          funct_verbose_message "svccfg -s svc:/system/environment:init setprop umask/umask = astring:  \"$umask_value\"" fix
-          funct_verbose_message "" fix
-        fi
-        if [ "$audit_mode" = 0 ]; then
-          echo "Setting:   Default service file creation mask to $umask_value"
-          if [ ! -f "$log_file" ]; then
-            echo "$umask_check" >> $log_file
+  if [ "$os_name" = "SunOS" ] || [ "$os_name" = "Linux" ] || ["$os_name" = "FreeBSD" ]; then
+    if [ "$os_name" = "SunOS" ]; then
+      if [ "$os_version" = "11" ]; then
+        funct_verbose_message "Daemon Umask"
+        umask_check=`svcprop -p umask/umask svc:/system/environment:init`
+        umask_value="022"
+        log_file="umask.log"
+        total=`expr $total + 1`
+        if [ "$umask_check" != "$umask_value" ]; then
+          log_file="$work_dir/$log_file"
+          if [ "$audit_mode" = 1 ]; then
+            score=`expr $score - 1`
+            echo "Warning:   Default service file creation mask not set to $umask_value [$score]"
+            funct_verbose_message "" fix
+            funct_verbose_message "svccfg -s svc:/system/environment:init setprop umask/umask = astring:  \"$umask_value\"" fix
+            funct_verbose_message "" fix
           fi
-          svccfg -s svc:/system/environment:init setprop umask/umask = astring:  "$umask_value"
-        fi
-      else
-        if [ "$audit_mode" = 1 ]; then
-          score=`expr $score + 1`
-          echo "Secure:    Default service file creation mask set to $umask_value [$score]"
-        fi
-        if [ "$audit_mode" = 2 ]; then
-          restore_file="$restore_dir/$log_file"
-          if [ -f "$restore_file" ]; then
-            restore_value=`cat $restore_file`
-            if [ "$restore_value" != "$umask_check" ]; then
-              echo "Restoring:  Default service file creation mask to $restore_vaule"
-              svccfg -s svc:/system/environment:init setprop umask/umask = astring:  "$restore_value"
+          if [ "$audit_mode" = 0 ]; then
+            echo "Setting:   Default service file creation mask to $umask_value"
+            if [ ! -f "$log_file" ]; then
+              echo "$umask_check" >> $log_file
+            fi
+            svccfg -s svc:/system/environment:init setprop umask/umask = astring:  "$umask_value"
+          fi
+        else
+          if [ "$audit_mode" = 1 ]; then
+            score=`expr $score + 1`
+            echo "Secure:    Default service file creation mask set to $umask_value [$score]"
+          fi
+          if [ "$audit_mode" = 2 ]; then
+            restore_file="$restore_dir/$log_file"
+            if [ -f "$restore_file" ]; then
+              restore_value=`cat $restore_file`
+              if [ "$restore_value" != "$umask_check" ]; then
+                echo "Restoring:  Default service file creation mask to $restore_vaule"
+                svccfg -s svc:/system/environment:init setprop umask/umask = astring:  "$restore_value"
+              fi
             fi
           fi
         fi
-      fi
-    else
-      if [ "$os_version" = "7" ] || [ "$os_version" = "6" ]; then
-        funct_verbose_message "Daemon Umask"
-        check_file="/etc/init.d/umask.sh"
-        funct_file_value $check_file umask space 022 hash
-        if [ "$audit_mode" = "0" ]; then
-          if [ -f "$check_file" ]; then
-            funct_check_perms $check_file 0744 root sys
-            for dir_name in /etc/rc?.d; do
-              link_file="$dir_name/S00umask"
-              if [ ! -f "$link_file" ]; then
-                ln -s $check_file $link_file
-              fi
-            done
-          fi
-        fi
       else
-        check_file="/etc/default/init"
-        funct_file_value $check_file CMASK eq 022 hash
+        if [ "$os_version" = "7" ] || [ "$os_version" = "6" ]; then
+          funct_verbose_message "Daemon Umask"
+          check_file="/etc/init.d/umask.sh"
+          funct_file_value $check_file umask space 022 hash
+          if [ "$audit_mode" = "0" ]; then
+            if [ -f "$check_file" ]; then
+              funct_check_perms $check_file 0744 root sys
+              for dir_name in /etc/rc?.d; do
+                link_file="$dir_name/S00umask"
+                if [ ! -f "$link_file" ]; then
+                  ln -s $check_file $link_file
+                fi
+              done
+            fi
+          fi
+        else
+          check_file="/etc/default/init"
+          funct_file_value $check_file CMASK eq 022 hash
+        fi
       fi
     fi
-  fi
-  if [ "$os_name" = "Linux" ]; then
-    funct_verbose_message "Daemon Umask"
-    check_file="/etc/sysconfig/init"
-    funct_file_value $check_file umask space 027 hash
-    if [ "$audit_mode" = "0" ]; then
-      if [ -f "$check_file" ]; then
-        funct_check_perms $check_file 0755 root root
+    if [ "$os_name" = "Linux" ]; then
+      check_file="/etc/sysconfig/init"
+      funct_file_value $check_file umask space 027 hash
+      if [ "$audit_mode" = "0" ]; then
+        if [ -f "$check_file" ]; then
+          funct_check_perms $check_file 0755 root root
+        fi
       fi
+    fi
+    if [ "$os_name" = "FreeBSD" ]; then
+      for check_file in `find /etc -type f |xargs grep 'umask' |cut -f1 -d:`; do
+        if -f [ "$check_file" ]; then
+          funct_file_value $check_file umask space 077 hash
+        fi
+      done
+      for check_file in `find /usr/local/etc -type f |xargs grep 'umask' |cut -f1 -d:`; do
+        if -f [ "$check_file" ]; then
+          funct_file_value $check_file umask space 077 hash
+        fi
+      done
     fi
   fi
 }
