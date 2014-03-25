f427e198acc3db0cefd330b02f1e745e89277b46
Richard Spindler <richard@lateralblast.com.au>
2014-03-25 22:18:47 +1100

    Added unowned files test and CIS reference for FreeBSD

--- lunar.sh
+++ lunar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Name:         lunar (Lockdown UNIX Analyse Report)
-# Version:      3.7.3
+# Version:      3.7.5
 # Release:      1
 # License:      Open Source
 # Group:        System
--- modules/audit_unowned_files.sh
+++ modules/audit_unowned_files.sh
@@ -7,10 +7,11 @@
 # was intended.
 #
 # Refer to Section 9.1.11-2 Page(s) 160-1 CIS CentOS Linux 6 Benchmark v1.0.0
+# Refer to Section(s) 6.7 Page(s) 23 CIS FreeBSD Benchmark v1.0.5
 #.
 
 audit_unowned_files () {
-  if [ "$os_name" = "SunOS" ] || [ "$os_name" = "Linux" ]; then
+  if [ "$os_name" = "SunOS" ] || [ "$os_name" = "Linux" ] || [ "$os_name" = "FreeBSD" ]; then
     funct_verbose_message "Unowned Files and Directories"
     if [ "$audit_mode" = 1 ]; then
       echo "Checking:  For Un-owned files and directories [This might take a while]"
