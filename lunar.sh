952ab52252450846db157a9b3118575dd1d9c7d5
Richard Spindler <richard@lateralblast.com.au>
2014-03-25 16:40:27 +1100

    Added printing test and CIS reference for FreeBSD

--- lunar.sh
+++ lunar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Name:         lunar (Lockdown UNIX Analyse Report)
-# Version:      3.6.1
+# Version:      3.6.2
 # Release:      1
 # License:      Open Source
 # Group:        System
--- modules/audit_print.sh
+++ modules/audit_print.sh
@@ -8,18 +8,26 @@
 # If the machine does not have locally attached printers,
 # disable this service.
 # Note that this service is not required for printing to a network printer.
+#
+# Refer to Section 3.14 Page(s) 14-15 CIS FreeBSD Benchmark v1.0.5
 #.
 
 audit_print () {
-  if [ "$os_name" = "SunOS" ]; then
-    if [ "$os_version" = "10" ]; then
-      funct_verbose_message "Printing Daemons"
-      service_name="svc:/application/print/ipp-listener:default"
-      funct_service $service_name disabled
-      service_name="svc:/application/print/rfc1179"
-      funct_service $service_name disabled
-      service_name="svc:/application/print/server:default"
-      funct_service $service_name disabled
+  if [ "$os_name" = "SunOS" ] || [ "$os_name" = "FreeBSD" ]; then
+    funct_verbose_message "Printing Daemons"
+    if [ "$os_name" = "SunOS" ]; then
+      if [ "$os_version" = "10" ]; then
+        service_name="svc:/application/print/ipp-listener:default"
+        funct_service $service_name disabled
+        service_name="svc:/application/print/rfc1179"
+        funct_service $service_name disabled
+        service_name="svc:/application/print/server:default"
+        funct_service $service_name disabled
+      fi
+    fi
+    if [ "$os_name" = "FreeBSD" ]; then
+      check_file="/etc/rc.conf"
+      funct_file_value $check_file lpd_enable eq NO hash
     fi
   fi
 }
