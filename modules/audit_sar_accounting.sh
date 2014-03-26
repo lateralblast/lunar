9199aa551a107611b55a1aad9308620dc39500f4
Richard Spindler <richard@lateralblast.com.au>
2014-03-27 10:56:15 +1100

    Added sar accounting test and CIS reference for AIX

---
+++ modules/audit_sar_accounting.sh
@@ -0,0 +1,50 @@
+# audit_sar_accounting
+#
+# The recommendation is to enable sar performance accounting.
+# This will provide a normal performance baseline which will help identify
+# unusual performance patterns, created through potential attacks via a
+# password cracking program being executed or through a DoS attack etc.
+# System accounting gathers periodic baseline system data, such as CPU
+# utilization and disk I/O. Once a normal baseline for the system has been
+# established, unauthorized activities, such as a password cracking being
+# executed and activity outside of normal usage hours may be detected due
+# to departure from the normal system performance baseline. It is recommended
+# that the collection script is run on an hourly basis, every day, to help to
+# detect any anomalies. It is also important to generate and review the system
+# activity report on a daily basis.
+#
+# Refer to Section(s) 2.12.8 Page(s) 212-3 CIS AIX Benchmark v1.1.0
+#.
+
+audit_sar_accounting() {
+  if [ "$os_name" = "SunOS" ] || [ "$os_name" = "AIX" ]; then
+    if [ "$os_name" = "AIX" ]; then
+      package_name="bos.acct"
+      funct_lslpp_check $package_name
+      if [ "$lslpp_check" = "$package_name" ]; then
+        check_file="/var/spool/cron/crontabs/adm"
+        funct_append_file $check_file "#================================================================="
+        funct_append_file $check_file "#      SYSTEM ACTIVITY REPORTS"
+        funct_append_file $check_file "# 8am-5pm activity reports every 20 mins during weekdays."
+        funct_append_file $check_file "# activity reports every an hour on Saturday and Sunday."
+        funct_append_file $check_file "# 6pm-7am activity reports every an hour during weekdays."
+        funct_append_file $check_file "# Daily summary prepared at 18:05."
+        funct_append_file $check_file "#================================================================="
+        funct_append_file $check_file "0 8-17 * * 1-5 /usr/lib/sa/sa1 1200 3 &"
+        funct_append_file $check_file "0 * * * 0,6 /usr/lib/sa/sa1 &"
+        funct_append_file $check_file "0 18-7 * * 1-5 /usr/lib/sa/sa1 &"
+        funct_append_file $check_file "5 18 * * 1-5 /usr/lib/sa/sa2 -s 8:00 -e 18:01 -i 3600 -ubcwyaqvm &"
+        $check_dir="/var/adm/sa"
+        if [ ! -d "$check_dir" ]; then
+          mkdir -p $check_dir
+        fi
+        funct_check_perms $check_dir 0755 adm adm
+      else
+        funct_verbose_message "" fix
+        funct_verbose_message "Sar accounting requires $package_name to be installed" fix
+        funct_verbose_message "" fix
+      fi
+    fi
+  fi
+}
+
--- modules/full_audit_accounting_services.sh
+++ modules/full_audit_accounting_services.sh
@@ -3,8 +3,9 @@
 # Audit accounting services
 #.
 
-full_audit_accounting_services () {
+full_audit_accounting_services() {
   audit_system_accounting
   audit_process_accounting
   audit_audit_class
+  audit_sar_accounting
 }
