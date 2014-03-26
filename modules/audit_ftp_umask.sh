91d5bd41088d4e83541ae07b45347ab1913379b9
Richard Spindler <richard@lateralblast.com.au>
2014-03-27 10:13:45 +1100

    Added FTP daemon umask test and CIS reference for AIX

--- modules/audit_ftp_umask.sh
+++ modules/audit_ftp_umask.sh
@@ -5,18 +5,26 @@
 # Many users assume that files transmitted over FTP inherit their system umask
 # value when they do not. This setting ensures that files transmitted over FTP
 # are protected.
+#
+# Refer to Section(s) 2.12.10 Page(s) 214-5 CIS AIX Benchmark v1.1.0
 #.
 
 audit_ftp_umask () {
-  if [ "$os_name" = "SunOS" ]; then
-    funct_verbose_message "Default umask for FTP Users"
-    if [ "$os_version" = "10" ]; then
-      check_file="/etc/ftpd/ftpaccess"
-      funct_file_value $check_file defumask space 077 hash
+  if [ "$os_name" = "SunOS" ] || [ "$os_name" = "AIX" ]; then
+    if [ "$os_name" = "AIX" ]; then
+      check_file="/etc/inetd.conf"
+      funct_file_value $check_file /usr/sbin/ftpd space "ftpd -l -u077" hash
     fi
-    if [ "$os_version" = "11" ]; then
-      check_file="/etc/proftpd.conf"
-      funct_file_value $check_file Umask space 027 hash
+    if [ "$os_name" = "SunOS" ]; then
+      funct_verbose_message "Default umask for FTP Users"
+      if [ "$os_version" = "10" ]; then
+        check_file="/etc/ftpd/ftpaccess"
+        funct_file_value $check_file defumask space 077 hash
+      fi
+      if [ "$os_version" = "11" ]; then
+        check_file="/etc/proftpd.conf"
+        funct_file_value $check_file Umask space 027 hash
+      fi
     fi
   fi
 }
