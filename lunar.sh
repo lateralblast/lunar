73019cedd12a1f1ccdb77a5da95757ad219a2c29
Richard Spindler <richard@lateralblast.com.au>
2014-03-28 10:22:21 +1100

    Added rwhod test and CIS reference for AIX

--- lunar.sh
+++ lunar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Name:         lunar (Lockdown UNIX Analyse Report)
-# Version:      4.3.9
+# Version:      4.4.0
 # Release:      1
 # License:      Open Source
 # Group:        System
--- modules/audit_remote_info.sh
+++ modules/audit_remote_info.sh
@@ -1,22 +1,29 @@
 # audit_remote_info
 #
 # Turn off remote info services like rstat and finger
+#
+# Refer to Section(s) 1.3.16 Page(s) 52-3 CIS AIX Benchmark v1.1.0
 #.
 
 audit_remote_info () {
-  if [ "$os_name" = "SunOS" ]; then
-    if [ "$os_version" = "10" ] || [ "$os_version" = "11" ]; then
-      funct_verbose_message "Remote Information Services"
-      service_name="svc:/network/rpc/rstat:default"
-      funct_service $service_name disabled
-      service_name="svc:/network/nfs/rquota:default"
-      funct_service $service_name disabled
-      service_name="svc:/network/rpc/rusers:default"
-      funct_service $service_name disabled
-      service_name="svc:/network/finger:default"
-      funct_service $service_name disabled
-      service_name="svc:/network/rpc/wall:default"
-      funct_service $service_name disabled
+  if [ "$os_name" = "SunOS" ] || [ "$os_name" = "AIX" ]; then
+    funct_verbose_message "Remote Information Services"
+    if [ "$os_name" = "AIX" ]; then
+      funct_rctcp_check rwhod off
+    fi
+    if [ "$os_name" = "SunOS" ]; then
+      if [ "$os_version" = "10" ] || [ "$os_version" = "11" ]; then
+        service_name="svc:/network/rpc/rstat:default"
+        funct_service $service_name disabled
+        service_name="svc:/network/nfs/rquota:default"
+        funct_service $service_name disabled
+        service_name="svc:/network/rpc/rusers:default"
+        funct_service $service_name disabled
+        service_name="svc:/network/finger:default"
+        funct_service $service_name disabled
+        service_name="svc:/network/rpc/wall:default"
+        funct_service $service_name disabled
+      fi
     fi
   fi
 }
