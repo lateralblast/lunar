50b8d52e3426cef12b5f18d0740b82258bca231a
Richard Spindler <richard@lateralblast.com.au>
2014-03-27 15:05:50 +1100

    Added NCS test and CIS reference for AIX

--- lunar.sh
+++ lunar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Name:         lunar (Lockdown UNIX Analyse Report)
-# Version:      4.2.0
+# Version:      4.2.1
 # Release:      1
 # License:      Open Source
 # Group:        System
