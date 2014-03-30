8fd69e4af10047bfdbf9b196d66063a7a5d5e005
Richard Spindler <richard@lateralblast.com.au>
2014-03-30 18:20:22 +1100

    Added kernel-PAE package check

--- lunar.sh
+++ lunar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Name:         lunar (Lockdown UNIX Analyse Report)
-# Version:      4.5.6
+# Version:      4.5.7
 # Release:      1
 # License:      Open Source
 # Group:        System
--- modules/audit_execshield.sh
+++ modules/audit_execshield.sh
@@ -8,15 +8,16 @@
 # enhances the security of the system.
 #
 # Refer to Section(s) 1.6.2 Page(s) 45-46 CIS CentOS Linux 6 Benchmark v1.0.0
-# Refer to Section(s) 1.6.2 Page(s) 51 CIS Red Hat Linux 5 Benchmark v2.1.0
+# Refer to Section(s) 1.6.2,4 Page(s) 51,52-3 CIS Red Hat Linux 5 Benchmark v2.1.0
 #.
 
 audit_execshield () {
   if [ "$os_name" = "Linux" ]; then
     if [ "$os_vendor" = "CentOS" ] || [ "$os_vendor" = "Red" ]; then
       if [ "$os_version" > 4 ]; then
-      check_file="/etc/sysctl.conf"
-      funct_file_vale $check_file kernel.exec-shield eq 1 hash
+        funct_linux_package install kernel-PAE
+        check_file="/etc/sysctl.conf"
+        funct_file_vale $check_file kernel.exec-shield eq 1 hash
     fi
   fi
 }
