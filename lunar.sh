768b274697196feb3546ae2540eae2ea4f6ced63
Richard Spindler <richard@lateralblast.com.au>
2017-01-14 16:25:46 +1100

    Initial Amazon Linux support

--- lunar.sh
+++ lunar.sh
@@ -225,7 +225,8 @@ check_os_release () {
         else
           if [ -f "/etc/os-release" ]; then
             os_vendor="Amazon"
-            os_version=`cat /etc/os-release |grep '^VERSION' |cut -f2 -d'"'`
+            os_version=`cat /etc/os-release |grep '^VERSION' |cut -f2 -d'"' |cut -f1 -d.`
+            os_update=`cat /etc/os-release |grep '^VERSION' |cut -f2 -d'"' |cut -f2 -d.`
           fi
         fi
       fi
