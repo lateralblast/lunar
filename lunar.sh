71031ab37f644de2ab01fec04d9b9e9a1ca62ed9
Richard Spindler <richard@lateralblast.com.au>
2017-01-19 07:35:36 +1100

    Added AWS support role check

--- lunar.sh
+++ lunar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Name:         lunar (Lockdown UNix Auditing and Reporting)
-# Version:      5.2.4
+# Version:      5.2.6
 # Release:      1
 # License:      CC-BA (Creative Commons By Attribution)
 #               http://creativecommons.org/licenses/by/4.0/legalcode
