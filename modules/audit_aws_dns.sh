dff4136bb04a2ea02a9baf68aea8365e24dcbc34
Richard Spindler <richard@lateralblast.com.au>
2017-01-25 08:33:02 +1100

    Added check for AWS Route53 Zone SPF records

--- modules/audit_aws_dns.sh
+++ modules/audit_aws_dns.sh
@@ -21,6 +21,17 @@
 # domains will allow you to reestablish full control over their registration.
 #
 # Refer to https://www.cloudconformity.com/conformity-rules/Route53/route-53-domain-expired.html
+# 
+# Ensure your AWS Route 53 hosted zones have a TXT DNS record that contains a
+# corresponding Sender Policy Framework (SPF) value set for each MX record
+# available. The SPF record enables your Route 53 registered domains to publicly
+# state which mail servers are authorized to send emails on its behalf.
+#
+# Implementing SPF (Sender Policy Framework) records for your AWS Route 53
+# domain names will help you detect and stop email address spoofing in
+# order to reduce spam and increase your domains trustworthiness.
+#
+# Refer to https://www.cloudconformity.com/conformity-rules/Route53/sender-policy-framework-record-present.html
 #.
 
 audit_aws_dns () {
@@ -48,6 +59,18 @@ audit_aws_dns () {
     else
       secure=`expr $secure + 1`
       echo "Secure:    Domain $domain registration has not expired [$secure Passes]"
+    fi
+  done
+  zones=`aws route53 list-hosted-zones --query "HostedZones[].Id" --output text 2> /dev/null |cut -f3 -d'/'`
+  for zone in $zones; do
+    total=`expr $total + 1`
+    spf=`aws route53 list-resource-record-sets --hosted-zone-id $zone --query "ResourceRecordSets[?Type == 'SPF']" --output text`
+    if [ "$spf" ]; then
+      secure=`expr $secure + 1`
+      echo "Secure:    Zone $zone has SPF records [$secure Passes]"
+    else
+      insecure=`expr $insecure + 1`
+      echo "Warning:   Zone $zone does not have SPF records [$insecure Warnings]" 
     fi
   done
 }
