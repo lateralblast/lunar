3d2e74b515058553a296f2432fe91ff399074b45
Richard Spindler <richard@lateralblast.com.au>
2017-01-26 09:18:24 +1100

    Added check for AWS SGs with open CIFS ports

--- modules/audit_aws_sgs.sh
+++ modules/audit_aws_sgs.sh
@@ -13,6 +13,48 @@
 # 
 # Removing unfettered connectivity to remote console services, such as RDP,
 # reduces a server's exposure to risk.
+#
+# Check your EC2 security groups for inbound rules that allow unrestricted
+# access (i.e. 0.0.0.0/0) to TCP port 22. Restrict access to only those IP
+# addresses that require it, in order to implement the principle of least
+# privilege and reduce the possibility of a breach. TCP port 22 is used for
+# secure remote login by connecting an SSH client application with an SSH
+# server.
+#
+# Allowing unrestricted SSH access can increase opportunities for malicious
+# activity such as hacking, man-in-the-middle attacks (MITM) and brute-force
+# attacks.
+#
+# Refer to https://www.cloudconformity.com/conformity-rules/EC2/unrestricted-ssh-access.html
+#
+# Check your EC2 security groups for inbound rules that allow unrestricted
+# access (i.e. 0.0.0.0/0) to TCP port 3389 and restrict access to only those IP
+# addresses that require it in order to implement the principle of least
+# privilege and reduce the possibility of a breach. TCP port 3389 is used for
+# secure remote GUI login to Microsoft servers by connecting an RDP (Remote
+# Desktop Protocol) client application with an RDP server.
+#
+# Allowing unrestricted RDP access can increase opportunities for malicious
+# activity such as hacking, man-in-the-middle attacks (MITM) and Pass-the-Hash
+# (PtH) attacks.
+#
+# Refer to https://www.cloudconformity.com/conformity-rules/EC2/unrestricted-rdp-access.html
+#
+# Check your EC2 security groups for inbound rules that allow unrestricted
+# access (i.e. 0.0.0.0/0) to TCP port 445 and restrict access to only those
+# IP addresses that require it in order to implement the principle of least
+# privilege and reduce the possibility of a breach. Common Internet File
+# System (CIFS) port 445 is used by client/server applications to provide
+# shared access to files, printers and communications between network nodes
+# directly over TCP (without NetBIOS) in Microsoft Windows Server 2003 and
+# later. CIFS is based on the enhanced version of Server Message Block (SMB)
+# protocol for internet/intranet file sharing, developed by Microsoft.
+#
+# Allowing unrestricted CIFS access can increase opportunities for malicious
+# activity such as man-in-the-middle attacks (MITM), Denial of Service (DoS)
+# attacks or the Windows Null Session Exploit.
+#
+# Refer to https://www.cloudconformity.com/conformity-rules/EC2/unrestricted-cifs-access.html
 #.
 
 audit_aws_sgs () {
@@ -26,6 +68,7 @@ audit_aws_sgs () {
     else
       funct_aws_open_port_check $sg 22 tcp SSH
       funct_aws_open_port_check $sg 3389 tcp RDP
+      funct_aws_open_port_check $sg 445 tcp CIFS
     fi
     outbound=`aws ec2 describe-security-groups --region $aws_region --group-ids $sg --filters Name=group-name,Values='default' --query 'SecurityGroups[*].{IpPermissionsEgress:IpPermissionsEgress,GroupId:GroupId}' |grep "0.0.0.0/0"`
     total=`expr $total + 1`
