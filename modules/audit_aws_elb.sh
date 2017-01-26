d774c05453bb03ffe6956d629b21ee30adf02eb4
Richard Spindler <richard@lateralblast.com.au>
2017-01-27 07:08:33 +1100

    Added check for AWS ELB using deprecated ciphers

--- modules/audit_aws_elb.sh
+++ modules/audit_aws_elb.sh
@@ -28,6 +28,24 @@ audit_aws_elb () {
       secure=`expr $secure + 1`
       echo "Secure:    ELB $elb has access logging enabled [$secure Passes]"
     fi
+    policies=`aws elb describe-load-balancer-policies --region $aws_region --region $aws_region --load-balancer-name $elb  --query "PolicyDescriptions[].PolicyName" --output text`
+    for policy in $policies; do
+      for cipher in RC2-CBC-MD5 PSK-AES256-CBC-SHA PSK-3DES-EDE-CBC-SHA KRB5-DES-CBC3-SHA KRB5-DES-CBC3-MD5 \
+                    PSK-AES128-CBC-SHA PSK-RC4-SHA KRB5-RC4-SHA KRB5-RC4-MD5 KRB5-DES-CBC-SHA KRB5-DES-CBC-MD5 \
+                    EXP-EDH-RSA-DES-CBC-SHA EXP-EDH-DSS-DES-CBC-SHA EXP-ADH-DES-CBC-SHA EXP-DES-CBC-SHA \
+                    EXP-RC2-CBC-MD5 EXP-KRB5-RC2-CBC-SHA EXP-KRB5-DES-CBC-SHA EXP-KRB5-RC2-CBC-MD5 \
+                    EXP-KRB5-DES-CBC-MD5 EXP-ADH-RC4-MD5 EXP-RC4-MD5 EXP-KRB5-RC4-SHA EXP-KRB5-RC4-MD5; do
+        total=`expr $total + 1`
+        check=`aws elb describe-load-balancer-policies --region $aws_region --load-balancer-name $elb |grep $cipher |grep true`
+        if [ "$check" ]; then
+          insecure=`expr $insecure + 1`
+          echo "Warning:   ELB $elb is using deprecated cipher $cipher [$insecure Warnings]"
+        else
+          secure=`expr $secure + 1`
+          echo "Secure:    ELB $elb is not using deprecated cipher $cipher [$secure Passes]"
+        fi
+      done
+    done
   done
 }
 
