e3fddc4bd5ec82c8238eb10fc0a9625b07be4d96
Richard Spindler <richard@lateralblast.com.au>
2017-01-29 20:53:00 +1100

    Added check for AWS ElastiCache having HA enabled

--- modules/audit_aws_ec.sh
+++ modules/audit_aws_ec.sh
@@ -19,13 +19,15 @@ audit_aws_ec () {
   for cache in $caches; do 
     total=`expr $total + 1`
     check=`aws elasticache describe-replication-groups --region $aws_region --replication-group-id $cache --query 'ReplicationGroups[].AutomaticFailover' |grep enabled`
-    stack=`echo "$stack" |cut -f2 -d/`
     if [ "$check" ]; then
       secure=`expr $secure + 1`
       echo "Secure:    ElastiCache $cache is Multi-AZ enabled [$secure Passes]"
     else
       insecure=`expr $insecure + 1`
       echo "Warning:   ElastiCache $cache is not Multi-AZ enabled [$insecure Warnings]"
+      funct_verbose_message "" fix
+      funct_verbose_message "aws elasticache modify-replication-group --region $aws_region --replication-group-id $cache --automatic-failover-enabled --apply-immediately" fix
+      funct_verbose_message "" fix
     fi
   done
 }
