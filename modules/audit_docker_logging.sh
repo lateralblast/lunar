437edb59f5412a13b81cb31a255772be6f2872d7
Richard Spindler <richard@lateralblast.com.au>
2017-02-08 08:37:43 +1100

    Added additional Docker logging tests

--- modules/audit_docker_logging.sh
+++ modules/audit_docker_logging.sh
@@ -2,8 +2,10 @@
 #
 # Check Docker logging
 #
-# Refer to Section(s) 2.2  Page(s) 38 CIS Docker Benchmark 1.13.0
+# Refer to Section(s) 2.2  Page(s) 38   CIS Docker Benchmark 1.13.0
 # Refer to https://docs.docker.com/engine/reference/commandline/daemon/
+# Refer to Section(s) 2.12 Page(s) 56-7 CIS Docker Benchmark 1.13.0
+# Refer to https://docs.docker.com/engine/admin/logging/overview/
 #.
 
 audit_docker_logging () {
@@ -11,7 +13,9 @@ audit_docker_logging () {
     docker_bin=`which docker`
     if [ "$docker_bin" ]; then
       funct_verbose_message "Docker Logging"
-      funct_dockerd_check unused log-level info
+      funct_dockerd_check unused daemon log-level info
+      funct_dockerd_check used daemon log-driver
+      funct_dockerd_check used daemon log-opt 
     fi
   fi
 }
