3d35edc951f92c5c1f4bf182850efa0e6a9a2b14
Richard Spindler <richard@lateralblast.com.au>
2017-02-08 12:46:46 +1100

    Added Docker SELinux test

--- modules/audit_docker_security.sh
+++ modules/audit_docker_security.sh
@@ -7,6 +7,10 @@
 # Refer to http://docs.docker.com/articles/security/#other-kernel-security-features
 # Refer to https://github.com/docker/docker/blob/master/docs/security/apparmor.md
 # Refer to http://docs.docker.com/reference/run/#security-configuration
+# Refer to Section(s) 5.1 Page(s) 128-9 CIS Docker Benchmark 1.13.0
+# Refer to http://docs.docker.com/articles/security/#other-kernel-security-features
+# Refer to http://docs.docker.com/reference/run/#security-configuration
+# Refer to http://docs.fedoraproject.org/en-US/Fedora/13/html/Security-Enhanced_Linux/
 #.
 
 audit_docker_security () {
@@ -16,16 +20,29 @@ audit_docker_security () {
       funct_verbose_message "Docker Security"
       if [ "$audit_mode" != 2 ]; then
         docker_info=`docker ps --quiet --all | xargs docker inspect --format '{{ .Id }}: User={{.Config.User }}' 2> /dev/null`
-        for armor_info in $docker_info; do
+        for info in $docker_info; do
           total=`expr $total + 1`
-          docker_id=`echo "$armor_info" |cut -f1 -d:`
-          profile=`echo "$armor_info" |cut -f2 -d: |cut -f2 -d=`
+          docker_id=`echo "$info" |cut -f1 -d:`
+          profile=`echo "$info" |cut -f2 -d: |cut -f2 -d=`
           if [ "$profile" ]; then
             secure=`expr $secure + 1`
-            echo "Secure:    Docker instance $docker_id has and AppArmor profile [$secure Passes]"
+            echo "Secure:    Docker instance $docker_id has an AppArmor profile [$secure Passes]"
           else
             insecure=`expr $insecure + 1`
             echo "Warning:   Docker instance $docker_id does not have an AppArmor profile [$insecure Warnings]"
+          fi
+        done
+        docker_info=`docker ps --quiet --all | xargs docker inspect --format '{{ .Id }}: SecurityOpt={{ .HostConfig.SecurityOpt }}' 2> /dev/null`
+        for info in $docker_info; do
+          total=`expr $total + 1`
+          docker_id=`echo "$info" |cut -f1 -d:`
+          profile=`echo "$info" |cut -f2 -d: |cut -f2 -d=`
+          if [ ! "$profile" = "<no value>" ]; then
+            secure=`expr $secure + 1`
+            echo "Secure:    Docker instance $docker_id has a SELinux profile [$secure Passes]"
+          else
+            insecure=`expr $insecure + 1`
+            echo "Warning:   Docker instance $docker_id does not have a SELinux profile [$insecure Warnings]"
           fi
         done
       fi
