4c035b5df39b713abb526ea619ffb2fbf0435547
Richard Spindler <richard@lateralblast.com.au>
2017-02-11 12:35:14 +1100

    Added addition system log tests for OS X and updated documentation

--- modules/audit_asl.sh
+++ modules/audit_asl.sh
@@ -2,13 +2,17 @@
 #
 # Check how long system logs are being kept for
 #
-# Refer to Section 3.1.1 Page(s) 85-6 CIS Apple OS X 10.8 Benchmark v1.0.0
+# Refer to Section 3.1.1 Page(s) 85-6  CIS Apple OS X 10.12 Benchmark v1.0.0
+# Refer to Section 3.1.2 Page(s) 87-8  CIS Apple OS X 10.12 Benchmark v1.0.0
+# Refer to Section 3.1.3 Page(s) 89-90 CIS Apple OS X 10.12 Benchmark v1.0.0
 #.
 
 audit_asl () {
   if [ "$os_name" = "Darwin" ]; then
     if [ "$os_release" -ge 12 ]; then
       check_append_file /etc/asl.conf "> system.log mode=0640 format=bsd rotate=utc compress file_max=5M ttl=90" hash
+      check_append_file /etc/asl.conf "> appfirewall.log mode=0640 format=bsd rotate=utc compress file_max=5M ttl=90" hash
+      check_append_file /etc/asl/com.apple.authd "* file /var/log/authd.log mode=0640 format=bsd rotate=utc compress file_max=5M ttl=90" hash
     fi
   fi
 }
