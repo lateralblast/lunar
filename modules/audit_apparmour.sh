6cd6618976f66732fc6de9c37033114f537406ad
Richard Spindler <richard@lateralblast.com.au>
2022-09-23 17:54:17 +1000

    Fixed AppArmor check for Ubuntu

--- modules/audit_apparmour.sh
+++ modules/audit_apparmour.sh
@@ -8,31 +8,34 @@
 audit_apparmour () {
   if [ "$os_name" = "Linux" ]; then
     do_test=0
+    package_name="apparmor"
+    app_name="AppArmor"
     if [ "$os_vendor" = "SuSE" ]; then 
+      check_file="/boot/grub/menu.lst"
       do_test=1
     fi
     if [ "$os_vendor" = "Ubuntu" ] && [ "$os_version" -ge 16 ]; then
+      check_file="/boot/grub/grub.cfg"
       do_test=1
     fi
     if [ "$do_test" = 1 ]; then
-      verbose_message "AppArmour"
-      check_linux_package install apparmour
-      check_file="/boot/grub/menu.lst"
+      verbose_message "$app_name"
+      check_linux_package install $package_name
       if [ "$audit_mode" = 2 ]; then
         restore_file $check_file $restore_dir
       else
         if [ -f "$check_file" ]; then
-          armour_test=$( grep "apparmour=0" $check_file | head -1 | wc -l )
+          armor_test=$( grep "$package_name=0" $check_file | head -1 | wc -l )
         else
-          armour_test=0
+          armor_test=0
         fi
-        if [ "$armour_test" = "1" ]; then
-          increment_insecure "AppArmour is not enabled"
-          temp_file="$temp_dir/apparmour"
+        if [ "$armor_test" = "1" ]; then
+          increment_insecure "$app_name is not enabled"
+          temp_file="$temp_dir/$package_name"
           backup_file $check_file
-          lockdown_command "cat $check_file |sed 's/apparmour=0//g' > $temp_file ; cat $temp_file > $check_file ; enforce /etc/apparmor.d/*" "Enabling AppArmour"
+          lockdown_command "cat $check_file |sed 's/$package_name=0//g' > $temp_file ; cat $temp_file > $check_file ; enforce /etc/$package_name.d/*" "Enabling $app_name"
         else
-          increment_secure "AppArmour enabled"
+          increment_secure "$app_name enabled"
         fi
       fi
     fi
