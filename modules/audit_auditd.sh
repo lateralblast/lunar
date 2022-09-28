64356d6af5340cdc521def19c2c9a85078bef0f0
Richard Spindler <richard@lateralblast.com.au>
2022-09-29 04:43:44 +1000

    Added audit log size check

--- modules/audit_auditd.sh
+++ modules/audit_auditd.sh
@@ -50,6 +50,45 @@ audit_auditd () {
           fi
         fi
       fi
+      package_name="audit_backlog_limit"
+      package_value="8192"
+      if [ "$audit_mode" = 2 ]; then
+        restore_file $check_file $restore_dir
+      else
+        if [ -f "$check_file" ]; then
+          package_disable_test=$( grep "$package_name=0" $check_file | head -1 | wc -l )
+          package_enabled_test=$( grep "$package_name=$package_value" $check_file | head -1 | wc -l )
+        else
+          package_disabled_test=0
+          package_enabled_test=0
+        fi
+        if [ "$package_disabled_test" ="1" ]; then
+          increment_insecure "$app_name is disabled in $check_file"
+          temp_file="$temp_dir/$package_name"
+          backup_file $check_file
+          lockdown_command "cat $check_file |sed 's/$package_name=0//g' > $temp_file ; cat $temp_file > $check_file ; update-grub" "Removing disabled $app_name in $check_file"
+          existing_value=`cat $check_file |grep "^GRUB_CMDLINE_LINUX" |cut -f2 -d= |sed "s/\"//g"`
+          new_value="GRUB_CMDLINE_LINUX=\"$package_name=$package_value $existing_value\""
+          lockdown_command "cat $check_file |sed 's/^GRUB_CMDLINE_LINUX/GRUB_CMDLINE_LINUX=\"$new_value\"/g' > $temp_file ; cat $temp_file > $check_file ; update-grub" "Enabling $app_name"
+        else
+          increment_secure "$app_name is not disabled in $check_file"
+        fi
+        if [ "$package_enabled_test" = "1" ]; then
+          increment_secure "$app_name is enabled $check_file"
+        else
+          increment_insecure "$app_name is not enabled in $check_file"
+          temp_file="$temp_dir/$package_name"
+          backup_file $check_file
+          line_check=$( grep "^GRUB_CMDLINE_LINUX" $check_file | head -1 | wc -l )
+          if [ "$line_check" = "1" ]; then
+            existing_value=`cat $check_file |grep "^GRUB_CMDLINE_LINUX" |cut -f2 -d= |sed "s/\"//g"`
+            new_value="GRUB_CMDLINE_LINUX=\"$package_name=$package_value $existing_value\""
+            lockdown_command "cat $check_file |sed 's/^GRUB_CMDLINE_LINUX/GRUB_CMDLINE_LINUX=\"$new_value\"/g' > $temp_file ; cat $temp_file > $check_file ; update-grub" "Enabling $app_name"
+          else
+            lockdown_command "echo 'GRUB_CMDLINE_LINUX=\"$package_name=$package_value\"' >> $check_file ; update-grub" "Enabling $app_name"
+          fi
+        fi
+      fi
     fi
     if [ "$os_name" = "Darwin" ]; then
       check_launchctl_service com.apple.auditd on
