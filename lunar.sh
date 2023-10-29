839bcaf130bc53d854ce46ae7576bd77584c0059
Richard Spindler <richard@lateralblast.com.au>
2023-10-29 21:32:22 +1100

    Added MacOS Time Machine check

--- lunar.sh
+++ lunar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Name:         lunar (Lockdown UNix Auditing and Reporting)
-# Version:      8.3.0
+# Version:      8.3.1
 # Release:      1
 # License:      CC-BA (Creative Commons By Attribution)
 #               http://creativecommons.org/licenses/by/4.0/legalcode
---
+++ modules/audit_time_machine.sh
@@ -0,0 +1,17 @@
+# audit_time_machine
+#
+# Backups should automatically run whenever the backup drive is available.
+#
+# Refer to Section(s) 2.3.4.1 Page(s) 125-8 CIS Apple macOS 14 Sonoma Benchmark v1.0.0
+#.
+
+audit_time_machine() {
+  if [ "$os_name" = "Darwin" ]; then
+    if [ "$os_version" -ge 14 ]; then
+      verbose_message "iCloud Drive"
+      if [ "$audit_mode" != 2 ]; then
+        check_osx_defaults /Library/Preferences/com.apple.TimeMachine.plist AutoBackup 1 bool
+      fi
+    fi
+  fi
+}
