7dd4f769fd43e685a42114438e2972e62911a70a
Richard Spindler <richard@lateralblast.com.au>
2023-10-31 10:32:12 +1100

    Added additional MocOS sleep checks

--- modules/audit_sleep.sh
+++ modules/audit_sleep.sh
@@ -1,11 +1,20 @@
 # audit_sleep
 #
-# Refer to Section 2.5.2 Page(s) 52-3 CIS Apple OS X 10.12 Benchmark v1.0.0
+# Refer to Section 2.5.2    Page(s) 52-3  CIS Apple OS X 10.12 Benchmark v1.0.0
+# Refer to Section 2.9.1.1  Page(s) 200-2 CIS Apple macOS 14 Sonoma Benchmark v1.0.0
 #.
 
 audit_sleep() {
   if [ "$os_name" = "Darwin" ]; then
     verbose_message "Sleep"
-    check_pmset sleep off
+    if [ "$os_version" -ge 14 ]; then
+      check_pmset standbydelaylow 900
+      check_pmset standbydelayhigh 900
+      check_pmset highstandbythreshold 90
+      check_pmset destroyfvkeyonstandby 1
+      check_pmset hibernatemode 25
+    else
+      check_pmset sleep off
+    fi
   fi
 }
