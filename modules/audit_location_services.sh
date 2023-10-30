ce449c6b5865ee82cec6b6b68063c2646626494d
Richard Spindler <richard@lateralblast.com.au>
2023-10-30 16:19:56 +1100

    Added additional location services check

--- modules/audit_location_services.sh
+++ modules/audit_location_services.sh
@@ -5,7 +5,7 @@
 # not be allowed if the physical location of the computer and the user should not be public
 # knowledge.
 #
-# Refer to Section(s) 2.6.1.1 Page(s) 153-5 CIS Apple macOS 14 Sonoma Benchmark v1.0.0
+# Refer to Section(s) 2.6.1.1-2 Page(s) 153-7 CIS Apple macOS 14 Sonoma Benchmark v1.0.0
 #.
 
 audit_location_services() {
@@ -14,6 +14,7 @@ audit_location_services() {
       verbose_message "Location Services"
       if [ "$audit_mode" != 2 ]; then
         check_osx_defaults /var/db/locationd/Library/Preferences/ByHost/com.apple.locationd LocationServicesEnabled 0 bool
+        check_osx_defaults /Library/Preferences/com.apple.locationmenu.plist ShowSystemServices 1 bool
       fi
     fi
   fi
