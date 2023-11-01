1393392e7ad815b17a92791f9d8abce34b82f13b
Richard Spindler <richard@lateralblast.com.au>
2023-11-02 10:01:50 +1100

    Updated MacOS password policy check for Sonoma

--- modules/audit_password_strength.sh
+++ modules/audit_password_strength.sh
@@ -2,9 +2,10 @@
 #
 # Refer to Section(s) 5.12-19 Page(s) 58-66  CIS Apple OS X 10.8 Benchmark v1.0.0
 # Refer to Section(s) 5.2.1-6 Page(s) 112-27 CIS Apple OS X 10.12 Benchmark v1.0.0
-# Refer to Section(s) 8.10    Page(s) verbose_message "     CIS FreeBSD Benchmark v1.0.5
+# Refer to Section(s) 8.10    Page(s)        CIS FreeBSD Benchmark v1.0.5
 # Refer to Section(s) 7.2     Page(s) 63-4   CIS Solaris 11.1 Benchmark v1.0.0
 # Refer to Section(s) 7.3     Page(s) 103-4  CIS Solaris 10 Benchmark v5.1.0
+# Refer to Section(s) 5.2.1-8 Page(s) 315-30 CIS Apple macOS 14 Sonoma Benchmark v1.0.0
 #.
 
 audit_password_strength () {
@@ -33,11 +34,15 @@ audit_password_strength () {
 #      check_pwpolicy minimumSymbolCharacters 1
       check_pwpolicy RequiresNumeric 1
 #      check_pwpolicy minimumNumericCharacters 1
+      if [ "$os_version" -ge 14 ]; then
+        check_pwpolicy requiresMixedCase 1
+        check_pwpolicy usingHistory 15
+      fi
       check_pwpolicy maxMinutesUntilChangePassword 86400
       check_pwpolicy minChars 15
       check_pwpolicy passwordCannotBeName 1
       check_pwpolicy minutesUntilFailedLoginReset 0
-      check_pwpolicy policyAttributeMaximumFailedAuthentications 5
+      check_pwpolicy policyAttributeMaximumFailedAuthentications 15
     fi
     if [ "$os_name" = "FreeBSD" ]; then
       check_file="/etc/login.conf"
