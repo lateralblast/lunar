ce795b0ff04817e4eadc6110345051b97f719322
Richard Spindler <richard@lateralblast.com.au>
2023-11-02 08:59:15 +1100

    Added MacOS Sealed System Volume check

--- modules/audit_system_integrity.sh
+++ modules/audit_system_integrity.sh
@@ -2,19 +2,28 @@
 #
 # Check System Integrity Protection is enabled
 #
-# Refer to Section(s) 5.18  Page(s) 148-9 CIS Apple OS X 10.12 Benchmark v1.0.0
-# Refer to Section(s) 5.1.2 Page(s) 300-3 CIS Apple macOS 14 Sonoma Benchmark v1.0.0
+# Refer to Section(s) 5.18    Page(s) 148-9     CIS Apple OS X 10.12 Benchmark v1.0.0
+# Refer to Section(s) 5.1.2,4 Page(s) 300-3,5-6 CIS Apple macOS 14 Sonoma Benchmark v1.0.0
 #.
 
 audit_system_integrity () {
   if [ "$os_name" = "Darwin" ]; then
     verbose_message "System Integrity"
     if [ "$audit_mode" != 2 ]; then
-      check=$( /usr/bin/csrutil status | grep enabled )
+      check=$( /usr/bin/csrutil status |grep enabled )
       if [ ! "$check" ]; then
         increment_insecure "System Integrity Protection is not enabled"
       else
         increment_secure "System Integrity Protection is enabled"
+      fi
+      if [ "$os_version" -ge 11 ]; then
+        verbose_message "Sealed System Volume"
+        check=$( /usr/bin/csrutil authenticated-root status |grep enabled )
+        if [ ! "$check" ]; then
+          increment_insecure "Sealed System Volume is not enabled"
+        else
+          increment_secure "Sealed System Volume is enabled"
+        fi
       fi
     fi
   fi
