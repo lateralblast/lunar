83607f7d3886582acd3812269c7d2bcb3918fe9e
Richard Spindler <richard@lateralblast.com.au>
2023-11-02 14:56:14 +1100

    Updated sudoers timeout check

--- modules/audit_sudo_timeout.sh
+++ modules/audit_sudo_timeout.sh
@@ -2,11 +2,21 @@
 #
 # Refer to Section(s) 5.1 Page(s) 48-9  CIS Apple OS X 10.8 Benchmark v1.0.0
 # Refer to Section(s) 5.3 Page(s) 128-9 CIS Apple OS X 10.12 Benchmark v1.0.0
+# Refer to Section(s) 5.4 Page(s) 343-5 CIS Apple macOS 14 Sonoma Benchmark v1.0.0
 #.
 
 audit_sudo_timeout () {
   if [ "$os_name" = "Darwin" ] || [ "$os_name" = "Linux" ] || [ "$os_name" = "SunOS" ]; then
-    check_file="/etc/sudoers"
+    if [ "$os_name" = "Darwin" ] && [ "$os_version" -ge 14 ]; then
+      check_file="/etc/sudoers.d/sudoers_timeout"
+    else
+      if [ "$os_name" = "Linux" ]; then
+        check_file="/etc/sudoers.d/sudoers_timeout"
+      else
+        check_file="/etc/sudoers"
+      fi
+    fi
     check_file_value is $check_file "Defaults timestamp_timeout" eq 0 hash after "# Defaults specification"
+    check_file_perms $check_file 440 root wheel 
   fi
 }
