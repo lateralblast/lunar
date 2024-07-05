6b307f152c8252c1d3a3182a23fb1a6a0cabc4fb
Richard Spindler <richard@lateralblast.com.au>
2024-07-05 21:01:13 +1000

    Added directory check to file append

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Do not use the lockdown functionality at the moment, it needs testing.
 Version
 -------
 
-Current version 9.1.2
+Current version 9.1.3
 
 Refer to lunar.sh and changelog for more up to date version information
 
--- functions/check_append_file.sh
+++ functions/check_append_file.sh
@@ -17,49 +17,57 @@ check_append_file () {
   check_file="$1"
   parameter="$2"
   comment_value="$3"
-  if [ "$comment_value" = "star" ]; then
-    comment_value="*"
-  else
-    comment_value="#"
+  dir_name=$( dirname "$check_file" )
+  if [ ! -f "$check_file" ]; then
+    verbose_message "File \"$check_file\" does not exist" "warn"
   fi
-  if [ "$audit_mode" = 2 ]; then
-    restore_file="$restore_dir$check_file"
-    restore_file "$check_file" "$restore_dir"
+  if [ ! -d "$dir_name" ]; then
+    verbose_message "Directory \"$dir_name\" does not exist" "warn"
   else
-    secure_string="Parameter \"$parameter\" is set in \"$check_file\""
-    insecure_string="Parameter \"$parameter\" is not set in \"$check_file\""
-    verbose_message "$secure_string" "check"
-    if [ ! -f "$check_file" ]; then
-      increment_insecure "Parameter \"$parameter\" does not exist in \"$check_file\""
-      lockdown_command   "echo \"$parameter\" >> $check_file"
-      if [ "$parameter" ]; then
-        if [ "$ansible" = 1 ]; then
-          echo ""
-          echo "- name: Checking $secure_string"
-          echo "  lineinfile:"
-          echo "    path: $check_file"
-          echo "    line: '$parameter'"
-          echo "    create: yes"
-          echo ""
-        fi
-      fi
+    if [ "$comment_value" = "star" ]; then
+      comment_value="*"
     else
-      if [ "$parameter" ]; then
-        if [ "$ansible" = 1 ]; then
-          echo ""
-          echo "- name: Checking $secure_string"
-          echo "  lineinfile:"
-          echo "    path: $check_file"
-          echo "    line: '$parameter'"
-          echo ""
+      comment_value="#"
+    fi
+    if [ "$audit_mode" = 2 ]; then
+      restore_file="$restore_dir$check_file"
+      restore_file "$check_file" "$restore_dir"
+    else
+      secure_string="Parameter \"$parameter\" is set in \"$check_file\""
+      insecure_string="Parameter \"$parameter\" is not set in \"$check_file\""
+      verbose_message "$secure_string" "check"
+      if [ ! -f "$check_file" ]; then
+        increment_insecure "Parameter \"$parameter\" does not exist in \"$check_file\""
+        lockdown_command   "echo \"$parameter\" >> $check_file"
+        if [ "$parameter" ]; then
+          if [ "$ansible" = 1 ]; then
+            echo ""
+            echo "- name: Checking $secure_string"
+            echo "  lineinfile:"
+            echo "    path: $check_file"
+            echo "    line: '$parameter'"
+            echo "    create: yes"
+            echo ""
+          fi
         fi
-        check_value=$( grep -v "^$comment_value" "$check_file" | grep -- "$parameter" )
-        if [ "$check_value" != "$parameter" ]; then
-          increment_insecure "$insecure_string"
-          backup_file        "$check_file"
-          lockdown_command   "echo \"$parameter\" >> $check_file"
-        else
-          increment_secure   "$secure_string"
+      else
+        if [ "$parameter" ]; then
+          if [ "$ansible" = 1 ]; then
+            echo ""
+            echo "- name: Checking $secure_string"
+            echo "  lineinfile:"
+            echo "    path: $check_file"
+            echo "    line: '$parameter'"
+            echo ""
+          fi
+          check_value=$( grep -v "^$comment_value" "$check_file" | grep -- "$parameter" )
+          if [ "$check_value" != "$parameter" ]; then
+            increment_insecure "$insecure_string"
+            backup_file        "$check_file"
+            lockdown_command   "echo \"$parameter\" >> $check_file"
+          else
+            increment_secure   "$secure_string"
+          fi
         fi
       fi
     fi
