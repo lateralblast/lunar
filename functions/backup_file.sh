e42c15c3a3dd596c1a70f4f9e0a19e62a2b37bfe
Richard Spindler <richard@lateralblast.com.au>
2024-07-07 10:08:38 +1000

    Improved file backup routine

--- functions/backup_file.sh
+++ functions/backup_file.sh
@@ -11,18 +11,22 @@
 
 backup_file () {
   check_file="$1"
-  if [ "$audit_mode" = 0 ]; then
-    backup_file="$work_dir$check_file"
-    if [ ! -f "$backup_file" ]; then
-      verbose_message "File \"$check_file\" to \"$backup_file\"" "backup"
-      find "$check_file" | cpio -pdm "$work_dir" 2> /dev/null
-      if [ "$check_file" = "/etc/system" ]; then
-        reboot=1
-      	verbose_message "Notice:    Reboot required"
-      fi
-      if [ "$check_file" = "/etc/ssh/sshd_config" ] || [ "$check_file" = "/etc/sshd_config" ]; then
-        verbose_message "Notice:    Service restart required for SSH"
+  if [ -f "$check_file" ]; then
+    if [ "$audit_mode" = 0 ]; then
+      backup_file="$work_dir$check_file"
+      if [ ! -f "$backup_file" ]; then
+        verbose_message "File \"$check_file\" to \"$backup_file\"" "backup"
+        find "$check_file" | cpio -pdm "$work_dir" 2> /dev/null
+        if [ "$check_file" = "/etc/system" ]; then
+          reboot=1
+        	verbose_message "Notice:    Reboot required"
+        fi
+        if [ "$check_file" = "/etc/ssh/sshd_config" ] || [ "$check_file" = "/etc/sshd_config" ]; then
+          verbose_message "Notice:    Service restart required for SSH"
+        fi
       fi
     fi
+  else
+    verbose_message "File \"$check_file\" does not exist"
   fi
 }
