08666a4105f97bcf4716ac18a4e0b9b087189785
Richard Spindler <richard@lateralblast.com.au>
2025-05-01 14:23:19 +1000

    Updated tests and documentation

--- modules/audit_ntp.sh
+++ modules/audit_ntp.sh
@@ -10,20 +10,22 @@
 #
 # Check NTP settings
 #
-# Refer to Section(s) 3.6       Page(s) 62-3   CIS CentOS Linux 6 Benchmark v1.0.0
-# Refer to Section(s) 3.6       Page(s) 75-6   CIS RHEL 5 Benchmark v2.1.0
-# Refer to Section(s) 3.6       Page(s) 65-6   CIS RHEL 6 Benchmark v1.2.0
-# Refer to Section(s) 2.2.1.1-2 Page(s) 98-101 CIS RHEL 7 Benchmark v2.1.0
-# Refer to Section(s) 2.4.5.1   Page(s) 35-6   CIS Apple OS X 10.5 Benchmark v1.1.0
-# Refer to Section(s) 2.2.1-3   Page(s) 26-31  CIS Apple OS X 10.12 Benchmark v1.0.0
-# Refer to Section(s) 2.3.2.1-2 Page(s) 82-6   CIS Apple macOS 14 Sonoma Benchmark v1.0.0
-# Refer to Section(s) 6.5       Page(s) 55-6   CIS SLES 11 Benchmark v1.0.0
-# Refer to Section(s) 1.9.2     Page(s) 16-7   CIS ESX Server 4 Benchmark v1.1.0
-# Refer to Section(s) 2.2.1.1-2 Page(s) 90-2   CIS Amazon Linux Benchmark v2.0.0
-# Refer to Section(s) 2.2.1.1-3 Page(s) 98-101 CIS Ubuntu 16.04 Benchmark v1.0.0
+# Refer to Section(s) 3.6           Page(s) 62-3    CIS CentOS Linux 6 Benchmark v1.0.0
+# Refer to Section(s) 3.6           Page(s) 75-6    CIS RHEL 5 Benchmark v2.1.0
+# Refer to Section(s) 3.6           Page(s) 65-6    CIS RHEL 6 Benchmark v1.2.0
+# Refer to Section(s) 2.2.1.1-2     Page(s) 98-101  CIS RHEL 7 Benchmark v2.1.0
+# Refer to Section(s) 2.4.5.1       Page(s) 35-6    CIS Apple OS X 10.5 Benchmark v1.1.0
+# Refer to Section(s) 2.2.1-3       Page(s) 26-31   CIS Apple OS X 10.12 Benchmark v1.0.0
+# Refer to Section(s) 2.3.2.1-2     Page(s) 82-6    CIS Apple macOS 14 Sonoma Benchmark v1.0.0
+# Refer to Section(s) 6.5           Page(s) 55-6    CIS SLES 11 Benchmark v1.0.0
+# Refer to Section(s) 1.9.2         Page(s) 16-7    CIS ESX Server 4 Benchmark v1.1.0
+# Refer to Section(s) 2.2.1.1-2     Page(s) 90-2    CIS Amazon Linux Benchmark v2.0.0
+# Refer to Section(s) 2.2.1.1-3     Page(s) 98-101  CIS Ubuntu 16.04 Benchmark v1.0.0
+# Refer to Section(s) 2.3.1.1-3.3.3 Page(s) 306-26  CIS Ubuntu 24.04 Benchmark v1.0.0
 #.
 
 audit_ntp () {
+  ntp_package="chrony"
   if [ "${os_name}" = "SunOS" ] || [ "${os_name}" = "Linux" ] || [ "${os_name}" = "Darwin" ] || [ "${os_name}" = "VMkernel" ]; then
     verbose_message "Network Time Protocol" "check"
     if [ "${os_name}" = "SunOS" ]; then
@@ -60,27 +62,44 @@ audit_ntp () {
       if [ "${os_vendor}" = "Amazon" ]; then
         do_chrony=1
       fi
-      if [ "${do_chrony}" -eq 1 ]; then
-        check_linux_package "install" "chrony"
+      if [ "${do_chrony}" -eq 1 ] && [ "${ntp_package}" = "chrony" ]; then
+        old_package="systemd-timesyncd"
+        check_linux_package "uninstall"       "${old_package}"
+        check_linux_service "${old_package}"  "off"
+        check_linux_package "install"         "${ntp_package}"
+        check_linux_service "${ntp_package}"  "on"
         check_file_value    "is"      "/etc/sysconfig/chronyd" "OPTIONS" "eq" "\"-u chrony\"" "hash"
         for server_number in $( seq 0 3 ); do
           ntp_server="${server_number}.${country_suffix}.pool.ntp.org"
           check_file_value "is" "/etc/chrony/chrony.conf" "pool" "${ntp_server} iburst" "space" "hash"
         done
       else
-        check_linux_package "install" "ntp"
-        if [ -f "/usr/bin/systemctl" ]; then
-          check_append_file "/usr/lib/systemd/system/ntpd.service"      "restrict default kod nomodify nopeer notrap noquery"    "hash"
-          check_append_file "/usr/lib/systemd/system/ntpd.service"      "restrict -6 default kod nomodify nopeer notrap noquery" "hash"
-          check_file_value  "is" "/usr/lib/systemd/system/ntpd.service" "OPTIONS"   "eq" "\"-u ntp:ntp -p /var/run/ntpd.pid\""   "hash"
-          check_file_value  "is" "/usr/lib/systemd/system/ntpd.service" "ExecStart" "eq" "/usr/sbin/ntpd -u ntp:ntp \$OPTIONS"   "hash"
+        if [ "${ntp_package}" = "systemd-timesyncd" ]; then
+          old_package="chrony"
+          check_linux_package "uninstall"       "${old_package}"
+          check_linux_service "${old_package}"  "off"
+          check_linux_package "install"         "${ntp_package}"
+          check_linux_service "${ntp_package}" "on"
+          comf_file="/etc/systemd/timesyncd.conf"
+          ntp_server="0.${country_suffix}.pool.ntp.org"
+          check_file_value "is" "${conf_file}" "NTP" "eq" "${ntp_server}" "hash"
+          ntp_server="1.${country_suffix}.pool.ntp.org"
+          check_file_value "is" "${conf_file}" "FallbackNTP" "eq" "${ntp_server}" "hash"
         else
-          check_linux_service "ntp" "on"
+          check_linux_package "install" "ntp"
+          if [ -f "/usr/bin/systemctl" ]; then
+            check_append_file "/usr/lib/systemd/system/ntpd.service"      "restrict default kod nomodify nopeer notrap noquery"    "hash"
+            check_append_file "/usr/lib/systemd/system/ntpd.service"      "restrict -6 default kod nomodify nopeer notrap noquery" "hash"
+            check_file_value  "is" "/usr/lib/systemd/system/ntpd.service" "OPTIONS"   "eq" "\"-u ntp:ntp -p /var/run/ntpd.pid\""   "hash"
+            check_file_value  "is" "/usr/lib/systemd/system/ntpd.service" "ExecStart" "eq" "/usr/sbin/ntpd -u ntp:ntp \$OPTIONS"   "hash"
+          else
+            check_linux_service "ntp" "on"
+          fi
+          for server_number in $( seq 0 3 ); do
+            ntp_server="${server_number}.${country_suffix}.pool.ntp.org"
+            check_file_value "is" "/etc/ntp.conf" "server" "space" "${ntp_server}" "hash"
+          done
         fi
-        for server_number in $( seq 0 3 ); do
-          ntp_server="${server_number}.${country_suffix}.pool.ntp.org"
-          check_file_value "is" "/etc/ntp.conf" "server" "space" "${ntp_server}" "hash"
-        done
       fi
     fi
   fi
