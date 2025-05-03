0373821c3c288933d824caaa83f1708d1b3ca4a1
Richard Spindler <richard@lateralblast.com.au>
2025-05-03 16:34:53 +1000

    Moved shellcheck function to core

--- lunar.sh
+++ lunar.sh
@@ -5,7 +5,7 @@
 # shellcheck disable=SC3046
 
 # Name:         lunar (Lockdown UNix Auditing and Reporting)
-# Version:      10.5.2
+# Version:      10.5.3
 # Release:      1
 # License:      CC-BA (Creative Commons By Attribution)
 #               http://creativecommons.org/licenses/by/4.0/legalcode
