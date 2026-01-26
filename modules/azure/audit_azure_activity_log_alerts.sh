9c39c679897ddc9d375563e157e494e55b6c8e95
Richard Spindler <richard@lateralblast.com.au>
2026-01-26 21:35:18 +1100

    Added Azure Activity Log Alerts check

---
+++ modules/azure/audit_azure_activity_log_alerts.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# shellcheck disable=SC1090
+# shellcheck disable=SC2034
+# shellcheck disable=SC2154
+
+# audit_azure_activity_log_alerts
+#
+# Check Azure Activity Log Alerts
+#
+# Refer to Section(s) 6.1.2.1 Page(s) 227-30 CIS Microsoft Azure Foundations Benchmark v5.0.0
+#.
+
+audit_azure_activity_log_alerts () {
+  print_function "audit_azure_activity_log_alerts"
+  verbose_message "Azure Activity Log Alerts" "check"
+  subscription_ids="$( az account show --query id --output tsv )"
+  for subscription_id in $subscription_ids; do
+    alert_check=$( az monitor activity-log alert list --subscription "${subscription_id}" --query "[].{Name:name,Enabled:enabled,Condition:condition.allOf,Actions:actions}" | grep "Microsoft.Authorization/policyAssignments/write" ) 
+    if [ -z "${alert_check}" ]; then
+      increment_secure "Activity Log Alert for Create Policy Assignment is enabled"
+    else
+      increment_insecure "Activity Log Alert for Create Policy Assignment is not enabled"
+    fi
+  done
+}
--- modules/azure/audit_azure_logging_and_monitoring.sh
+++ modules/azure/audit_azure_logging_and_monitoring.sh
@@ -24,6 +24,7 @@ audit_azure_logging_and_monitoring () {
   # 6.1.1.8 Ensure that a Microsoft Entra diagnostic setting exists to send Microsoft Graph activity logs to an appropriate destination
   # 6.1.1.9 Ensure that a Microsoft Entra diagnostic setting exists to send Microsoft Entra activity logs to an appropriate destination
   # 6.1.1.10 Ensure that Intune logs are captured and sent to Log Analytics
+  # 6.1.2.1 Ensure that Activity Log Alert exists for Create Policy Assignment
   audit_azure_subscription_diagnostic_settings
   audit_azure_diagnostic_setting_categories
   audit_azure_activity_logs_cmk
@@ -34,4 +35,5 @@ audit_azure_logging_and_monitoring () {
   audit_azure_graph_diagnostic_settings
   audit_azure_entra_diagnostic_settings
   audit_azure_intune_logs
+  audit_azure_activity_log_alerts
 }
