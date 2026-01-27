33d089b6ee0e3c4d4bc285ca7cdea60be5b4a2dd
Richard Spindler <richard@lateralblast.com.au>
2026-01-27 20:37:59 +1100

    Added check for Microsoft Defender for SQL Server

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Lockdown UNix Auditing and Reporting
 Version
 -------
 
-Current version 11.9.9
+Current version 12.0.0
 
 Refer to lunar.sh and changelog for more up to date version information
 
--- lunar.sh
+++ lunar.sh
@@ -7,7 +7,7 @@
 # shellcheck disable=SC3046
 
 # Name:         lunar (Lockdown UNix Auditing and Reporting)
-# Version:      11.9.9
+# Version:      12.0.0
 # Release:      1
 # License:      CC-BA (Creative Commons By Attribution)
 #               http://creativecommons.org/licenses/by/4.0/legalcode
