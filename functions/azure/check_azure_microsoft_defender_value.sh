c4b3b95dceaa2c708fc4c6e7ccda5d9a5f29e750
Richard Spindler <richard@lateralblast.com.au>
2026-01-27 14:42:02 +1100

    Added Azure Microsoft Defender check for CWP

--- functions/azure/check_azure_microsoft_defender_value.sh
+++ functions/azure/check_azure_microsoft_defender_value.sh
@@ -22,6 +22,14 @@ check_azure_microsoft_defender_value () {
     increment_secure   "Microsoft Defender \"${parameter_name}\" is set to \"${correct_value}\""
   else
     increment_insecure "Microsoft Defender \"${parameter_name}\" is not set to \"${correct_value}\""
-    verbose_message    "az security pricing update --name \"${parameter_name}\" --pricing-tier \"${correct_value}\"" "fix"
+    if [ "${parameter_name}" = "CloudPosture" ]; then
+      verbose_message    "az security pricing update --name \"${parameter_name}\" --tier \"${correct_value}\" --extensions name=ApiPosture isEnabled=true" "fix"
+    else
+      if [ "${parameter_name}" = "Api" ]; then
+        verbose_message    "az security pricing update --name \"${parameter_name}\" --tier \"${correct_value}\" --subplan <subplan>" "fix"
+      else
+        verbose_message    "az security pricing update --name \"${parameter_name}\" --tier \"${correct_value}\"" "fix"
+      fi
+    fi
   fi
 }
