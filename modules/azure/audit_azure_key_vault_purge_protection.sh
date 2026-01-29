a8deb648891b406201591e886623dda089d009e8
Richard Spindler <richard@lateralblast.com.au>
2026-01-29 17:37:05 +1100

    Added Azure Key Vault Purge Protection check

---
+++ modules/azure/audit_azure_key_vault_purge_protection.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# shellcheck disable=SC1090
+# shellcheck disable=SC2034
+# shellcheck disable=SC2154
+
+# audit_azure_key_vault_purge_protection
+#
+# Check Azure Key Vault Purge Protection
+#
+# 8.3.5 Ensure that Purge Protection is enabled for all Key Vaults
+# Refer to Section(s) 8.3.5 Page(s) 440-3 CIS Microsoft Azure Foundations Benchmark v5.0.0
+#
+# This requires the Azure CLI to be installed and configured
+# Audit account needs to have the 'Key Vault Reader' role
+# To do any changes to the key vaults, the 'Key Vault Administrator' role is required
+#.
+
+audit_azure_key_vault_purge_protection () {
+  print_function  "audit_azure_key_vault_purge_protection"
+  verbose_message "Azure Key Vault Purge Protection" "check"
+  resource_names=$( az resource list --query "[?type=='Microsoft.KeyVault/vaults'].name" --output tsv )
+  for resource_name in ${resource_names}; do
+    resource_groups=$( az resource list --name "${resource_name}" --query "[].resourceGroup" --output tsv )
+    for resource_group in ${resource_groups}; do
+      purge_protection=$( az resource show --resource-group "${resource_group}" --name "${resource_name}" --resource-type "Microsoft.KeyVault/vaults" --query "properties.enablePurgeProtection" --output tsv )
+      verbose_message "Azure Key Vault \"${resource_name}\" purge protection" "check"
+      if [ "${purge_protection}" = "true" ]; then
+        increment_secure   "Azure Key Vault \"${resource_name}\" purge protection is enabled"
+      else
+        increment_insecure "Azure Key Vault \"${resource_name}\" purge protection is not enabled"
+        verbose_message    "az resource update --resource-group ${resource_group} --name ${resource_name} --resource-type "Microsoft.KeyVault/vaults" --set properties.enablePurgeProtection=true" "fix"
+      fi
+    done
+  done
+}
+
