5358c9c5c6d6b27cfec1364e1bbcccece9f17321
Richard Spindler <richard@lateralblast.com.au>
2026-02-25 16:30:37 +1100

    Added Azure App Service App check for Minimum Inbound TLS Version

--- modules/azure/audit_azure_app_service_apps.sh
+++ modules/azure/audit_azure_app_service_apps.sh
@@ -50,18 +50,20 @@ audit_azure_app_service_apps () {
     command_message "${command}"
     resource_group=$( eval "${command}" )
     # 2.1.1   Ensure 'Java version' is currently supported (if in use) 
-    check_azure_app_service_app_value "Java Version"           "${app_name}" "${resource_group}" "javaVersion"          "eq" "${azure_java_version}"   "--java-version"           ""
-    check_azure_app_service_app_value "Java Container Version" "${app_name}" "${resource_group}" "javaContainerVersion" "eq" "${azure_java_version}"   "--java-container-version" ""
+    check_azure_app_service_app_value "Java Version"                "${app_name}" "${resource_group}" "javaVersion"          "eq" "${azure_java_version}"   "--java-version"           ""
+    check_azure_app_service_app_value "Java Container Version"      "${app_name}" "${resource_group}" "javaContainerVersion" "eq" "${azure_java_version}"   "--java-container-version" ""
     # 2.1.2   Ensure 'Python version' is currently supported (if in use) 
-    check_azure_app_service_app_value "Python Version"         "${app_name}" "${resource_group}" "pythonVersion"        "eq" "${azure_python_version}" "--python-version"         ""
+    check_azure_app_service_app_value "Python Version"              "${app_name}" "${resource_group}" "pythonVersion"        "eq" "${azure_python_version}" "--python-version"         ""
     # 2.1.3   Ensure 'PHP version' is currently supported (if in use)
-    check_azure_app_service_app_value "PHP Version"            "${app_name}" "${resource_group}" "phpVersion"           "eq" "${azure_php_version}"    "--php-version"            ""
+    check_azure_app_service_app_value "PHP Version"                 "${app_name}" "${resource_group}" "phpVersion"           "eq" "${azure_php_version}"    "--php-version"            ""
     # 2.1.5   Ensure 'FTP State' is set to 'FTPS only' or 'Disabled' 
-    check_azure_app_service_app_value "FTP State"              "${app_name}" "${resource_group}" "ftpState"             "eq" "${azure_ftp_state}"      "--ftp-state"              ""
+    check_azure_app_service_app_value "FTP State"                   "${app_name}" "${resource_group}" "ftpState"             "eq" "${azure_ftp_state}"      "--ftp-state"              ""
     # 2.1.6   Ensure 'HTTP version' is set to '2.0' (if in use)
-    check_azure_app_service_app_value "HTTP Version"           "${app_name}" "${resource_group}" "http20Enabled"        "eq" "true"                   "--http20-enabled"          ""
+    check_azure_app_service_app_value "HTTP Version"                "${app_name}" "${resource_group}" "http20Enabled"        "eq" "true"                    "--http20-enabled"         ""
     # 2.1.7   Ensure 'HTTPS Only' is set to 'On'
-    check_azure_app_service_app_value "HTTPS Only"             "${app_name}" "${resource_group}" "httpsOnly"            "eq" "true"                   "httpsOnly"                 ""
+    check_azure_app_service_app_value "HTTPS Only"                  "${app_name}" "${resource_group}" "httpsOnly"            "eq" "true"                    "httpsOnly"                ""
+    # 2.1.8   Ensure 'Minimum Inbound TLS Version' is set to '1.2' or higher
+    check_azure_app_service_app_value "Minimum Inbound TLS Version" "${app_name}" "${resource_group}" "minTlsVersion"        "eq" "1.2"                     "--min-tls-version"        ""
     # 2.1.4   Ensure 'Basic Authentication Publishing Credentials' are 'Disabled'
     check_azure_basic_authentication_publishing_credential_value "Basic Authentication Publishing Credentials" "${app_name}" "${resource_group}" "ftp" "Microsoft.Web" "basicPublishingCredentialsPolicies" "properties.allow" "eq" "false" "--auth-settings.publishing-credentials-enabled" ""
     check_azure_basic_authentication_publishing_credential_value "Basic Authentication Publishing Credentials" "${app_name}" "${resource_group}" "scm" "Microsoft.Web" "basicPublishingCredentialsPolicies" "properties.allow" "eq" "false" "--auth-settings.publishing-credentials-enabled" ""
