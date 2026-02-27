a234cce5f0c8de80d830efa3711eb1e97cb4b1bb
Richard Spindler <richard@lateralblast.com.au>
2026-02-27 18:07:54 +1100

    Added Azure Batch check for local authentication methods

--- modules/azure/audit_azure_batch.sh
+++ modules/azure/audit_azure_batch.sh
@@ -10,6 +10,7 @@
 #
 # 15.1 Ensure Batch account is set to use customer-managed keys to encrypt data 
 # 15.2 Ensure Batch pools disk encryption is set enabled
+# 15.3 Ensure local authentication methods for accounts are disabled
 #
 # Refer to Section(s) 15- Page(s) 277- CIS Microsoft Azure Compute Services Benchmark v2.0.0
 #
@@ -29,7 +30,8 @@ audit_azure_batch () {
     command="az batch account show --name \"${batch_name}\" --query \"[].resourceGroup\" --output tsv"
     command_message "${command}"
     resource_group=$( eval "${command}" )
-    check_azure_batch_value "Customer Managed Keys" "${batch_name}" "${resource_group}" "keyVaultReference" "ne" "" "--encryption-key-identifier" "https://<keyvault_name>.vault.azure.net/keys/<key_name>/<Version>"
+    check_azure_batch_value "Customer Managed Keys"        "${batch_name}" "${resource_group}" "keyVaultReference"          "ne" ""    "--encryption-key-identifier" "https://<keyvault_name>.vault.azure.net/keys/<key_name>/<Version>"
+    check_azure_batch_value "Local Authentication Methods" "${batch_name}" "${resource_group}" "allowedAuthenticationModes" "eq" "AAD" ""                            ""
     command="az batch pool list --account-name \"${batch_name}\" --query \"[].id\" --output tsv"
     command_message "${command}"
     pool_ids=$( eval "${command}" )
