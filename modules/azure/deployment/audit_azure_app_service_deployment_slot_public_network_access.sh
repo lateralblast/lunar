72587c9655c21cb339a7a5f2821d526e8aca3331
Richard Spindler <richard@lateralblast.com.au>
2026-03-16 17:07:56 +1100

    Split out Azure App Service Deployment Slot Public Network Access check

---
+++ modules/azure/deployment/audit_azure_app_service_deployment_slot_public_network_access.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# shellcheck disable=SC1090
+# shellcheck disable=SC2034
+# shellcheck disable=SC2154
+
+# audit_azure_app_service_deployment_slot_public_network_access
+#
+# 2.2.13  Ensure public network access is disabled - TBD
+#
+# Refer to Section(s) 2.2.13 Page(s) 128-30 CIS Microsoft Azure Compute Services Benchmark v2.0.0
+#
+# This requires the Azure CLI to be installed and configured
+#.
+
+audit_azure_app_service_deployment_slot_public_network_access () {
+  print_function "audit_azure_app_service_deployment_slot_public_network_access"
+  check_message  "Azure App Service Deployment Slot Public Network Access"
+  command="az webapp list --query \"[].id\" --output tsv"
+  command_message   "${command}"
+  app_ids=$( eval "${command}" 2> /dev/null )
+  if [ -z "${app_ids}" ]; then
+    info_message "No App Service Deployment Slots found"
+    return
+  fi
+  for app_id in ${app_ids}; do
+    command="az webapp show --id \"${app_id}\" --query \"name\" --output tsv"
+    command_message   "${command}"
+    app_name=$( eval  "${command}" )
+    command="az webapp show --id \"${app_id}\" --query \"resourceGroup\" --output tsv"
+    command_message   "${command}"
+    res_group=$( eval "${command}" )
+    command="az webapp deployment slot list --id \"${app_id}\" --query \"[].id\" --output tsv"
+    command_message   "${command}"
+    slot_ids=$( eval  "${command}" 2> /dev/null )
+    if [ -z "${slot_ids}" ]; then
+      info_message "No App Service Deployment Slots found"
+      return
+    fi
+    for slot_id in ${slot_ids}; do
+      check_azure_app_service_deployment_slot_value "Public Network Access" "${slot_id}" "${app_name}" "${res_group}" "config" "web" "Microsoft.Web/sites" "publicNetworkAccess" "eq" "Disabled" "properties.publicNetworkAccess" ""
+    done
+  done
+}
--- modules/azure/deployment/audit_azure_app_service_deployment_slots.sh
+++ modules/azure/deployment/audit_azure_app_service_deployment_slots.sh
@@ -62,6 +62,8 @@ audit_azure_app_service_deployment_slots () {
   audit_azure_app_service_deployment_slot_client_certificates
   # 2.2.12  Ensure managed identities are configured - TBD
   audit_azure_app_service_deployment_slot_managed_identities
+  # 2.2.13  Ensure public network access is disabled - TBD
+  audit_azure_app_service_deployment_slot_public_network_access
   for app_name in ${app_names}; do
     command="az webapp show --name \"${app_name}\" --query \"resourceGroup\" --output tsv"
     command_message    "${command}"
@@ -74,8 +76,6 @@ audit_azure_app_service_deployment_slots () {
       return
     fi
     for slot_name in ${slot_names}; do
-      # 2.2.13  Ensure public network access is disabled - TBD
-      check_azure_app_service_deployment_slot_value "Public Network Access"                       "${slot_id}" "${app_name}" "${res_group}" "config"                             "web" "Microsoft.Web/sites" "publicNetworkAccess"               "eq" "Disabled"                      "properties.publicNetworkAccess"        ""
       # 2.2.14  Ensure app is integrated with a virtual network - TBD
       check_azure_app_service_deployment_slot_value "Virtual Network Integration"                 "${slot_id}" "${app_name}" "${res_group}" "config"                             "web" "Microsoft.Web/sites" "virtualNetworkSubnetId"            "ne" ""                              ""                                      ""
       # 2.2.15  Ensure configuration is routed through the virtual network integration - TBD
