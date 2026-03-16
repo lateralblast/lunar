0d82f54e543e237dbdcba5c674fdd3d15d9adf9f
Richard Spindler <richard@lateralblast.com.au>
2026-03-16 16:56:58 +1100

    Split out Azure App Service Deployment Slot Client Certificates check

---
+++ modules/azure/deployment/audit_azure_app_service_deployment_slot_client_certificates.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# shellcheck disable=SC1090
+# shellcheck disable=SC2034
+# shellcheck disable=SC2154
+
+# audit_azure_app_service_deployment_slot_client_certificates
+#
+# 2.2.11  Ensure incoming client certificates are enabled and required (if in use) - TBD
+#
+# Refer to Section(s) 2.2.11 Page(s) 122-4 CIS Microsoft Azure Compute Services Benchmark v2.0.0
+#
+# This requires the Azure CLI to be installed and configured
+#.
+
+audit_azure_app_service_deployment_slot_client_certificates () {
+  print_function "audit_azure_app_service_deployment_slot_client_certificates"
+  check_message  "Azure App Service Deployment Slot Client Certificates"
+  command="az webapp list --query \"[].id\" --output tsv"
+  command_message   "${command}"
+  app_ids=$( eval "${command}" 2> /dev/null )
+  if [ -z "${app_ids}" ]; then
+    info_message "No App Service Deployment Slots found"
+    return
+  fi
+  for app_id in ${app_ids}; do
+    command="az webapp show --id \"${app_id}\" --query \"name\" --output tsv"
+    command_message   "${command}"
+    app_name=$( eval  "${command}" )
+    command="az webapp show --id \"${app_id}\" --query \"resourceGroup\" --output tsv"
+    command_message   "${command}"
+    res_group=$( eval "${command}" )
+    command="az webapp deployment slot list --id \"${app_id}\" --query \"[].id\" --output tsv"
+    command_message   "${command}"
+    slot_ids=$( eval  "${command}" 2> /dev/null )
+    if [ -z "${slot_ids}" ]; then
+      info_message "No App Service Deployment Slots found"
+      return
+    fi
+    for slot_id in ${slot_ids}; do
+      check_azure_app_service_deployment_slot_value "Client Certificates" "${slot_id}" "${app_name}" "${res_group}" "config" "web" "Microsoft.Web/sites" "clientCertEnabled" "eq" "true" "clientCertEnabled" ""
+    done
+  done
+}
--- modules/azure/deployment/audit_azure_app_service_deployment_slots.sh
+++ modules/azure/deployment/audit_azure_app_service_deployment_slots.sh
@@ -58,6 +58,8 @@ audit_azure_app_service_deployment_slots () {
   audit_azure_app_service_deployment_slot_tls_values
   # 2.2.10  Ensure 'Remote debugging' is set to 'Off' - TBD
   audit_azure_app_service_deployment_slot_remote_debugging
+  # 2.2.11  Ensure incoming client certificates are enabled and required (if in use) - TBD
+  audit_azure_app_service_deployment_slot_client_certificates
   for app_name in ${app_names}; do
     command="az webapp show --name \"${app_name}\" --query \"resourceGroup\" --output tsv"
     command_message    "${command}"
@@ -70,8 +72,6 @@ audit_azure_app_service_deployment_slots () {
       return
     fi
     for slot_name in ${slot_names}; do
-      # 2.2.11  Ensure incoming client certificates are enabled and required (if in use) - TBD
-      check_azure_app_service_deployment_slot_value "Client Certificates"                         "${slot_id}" "${app_name}" "${res_group}" "config"                             "web" "Microsoft.Web/sites" "clientCertEnabled"                 "eq" "true"                          "clientCertEnabled"                     ""
       # 2.2.12  Ensure managed identities are configured - TBD
       check_azure_app_service_deployment_slot_value "Managed Identities"                          "${slot_id}" "${app_name}" "${res_group}" "config"                             "web" "identity"            "type"                              "eq" "${azure_managed_identity}"     ""                                      ""
       # 2.2.13  Ensure public network access is disabled - TBD
